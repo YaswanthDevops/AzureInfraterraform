9047351e8d2fa86971100199dd8090f5609e02d3
yaswanth <yeshwanth.vardhineni@gmail.com>
2025-02-25 16:04:39 +0000

    backend data updated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "NetworkWatcherRG"
-    storage_account_name = "yashstorage98764567"
+    storage_account_name = "yashstorage987654321"
     container_name       = "yashbackendcontainer7654"
     key                  = "terraform.tfstate"
   }
