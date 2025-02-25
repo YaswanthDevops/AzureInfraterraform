6cd08483396f6dbb220907a7be39d5bbb37a25dd
yaswanth <yeshwanth.vardhineni@gmail.com>
2025-02-25 15:59:10 +0000

    backend storage RG modified

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "my-terraform-rg"
+    resource_group_name  = "NetworkWatcherRG"
     storage_account_name = "yashstorage98764567"
     container_name       = "yashbackendcontainer7654"
     key                  = "terraform.tfstate"
